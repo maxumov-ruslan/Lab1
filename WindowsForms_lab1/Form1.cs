440ef4b97d29a192739189e0e1cfd64d70539574
maxumov.ruslan@mail.ru <maxumov.ruslan@mail.ru>
2021-10-17 18:38:05 +0300

    Отправление на гитхаб

--- WindowsForms_lab1/Form1.cs
+++ WindowsForms_lab1/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsForms_lab1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
     }
 }
